6635228a097770c56a3d8ad55bd650bb0f94c233
amonge20 <2023_aitor.monge@iticbcn.cat>
2025-09-12 16:30:46 +0200

    Script de automatizacion de tareas, casi terminado

--- Inicio.py
+++ Inicio.py
@@ -135,35 +135,75 @@ else:
     print("El fichero 'archivo2.txt' ya creado")
 
 
-# "Abrimos" 'archivoFinal.txt' y escribira lo siguiente (Estoy en el punto 15 y 16 de un ejercicio)
+# "Abrimos" 'archivoFinal.txt' y escribira lo siguiente
+with open('archivoFinal.txt','w', encoding="utf-8") as f:
+    f.write("esto es una prueba 1\n")
+    f.write("esto es una prueba 2\n")
+    f.write("esto es una prueba 3\n")
+    f.write("esto es una prueba 4\n")
+    f.write("esto es una prueba 5\n")
+    f.write("esto es una prueba 6\n")
+    f.write("esto es una prueba 7\n")
+    
+with open('archivoFinal.txt','r') as r:
+    contenido_archivoFinal = r.read() # Mostrar el contenido escrito
+    print("Mostrando el contenido del fichero 'archivoFinal.txt'")
+    print(contenido_archivoFinal)
+    
+# "Abrimos" 'archivo2.txt' y escribira lo siguiente 
+with open('archivo2.txt','w', encoding="utf-8") as f:
+    f.write("esto es una prueba 8\n")
+    f.write("esto es una prueba 9\n")
+    f.write("esto es una prueba 10\n")
+    f.write("esto es una prueba 11\n")
+    f.write("esto es una prueba 12\n")
+    f.write("esto es una prueba 13\n")
+    f.write("esto es una prueba 14\n")
+    
+with open('archivo2.txt','r') as r:
+    contenido_archivo2 = r.read() # Mostrar el contenido escrito
+    print("Mostrando el contenido del fichero 'archivo2.txt'")
+    print(contenido_archivo2)
+
+print('\n')
+print("Concatenación de archivos con 'archivoFinal.txt' y 'archivo2.txt'")
+# Concatena ambos archivos ('archivoFinal.txt', 'archivo2.txt') y crea como resultado un nuevo archivo llamado Resultado.txt 
+archivos = ['archivoFinal.txt','archivo2.txt']
+
+archivo_salida = 'Resultado.txt'
+
+with open(archivo_salida,'w', encoding="utf-8") as salida:
+    for nombre in archivos:
+        try:
+            with open(nombre, 'r', encoding='utf-8') as entrada:
+              contenido = entrada.read()
+              salida.write(contenido)
+              salida.write('\n')  
+        except FileNotFoundError:
+            print("Archivo no encontrado: {nombre}")
+        except Exception as e:
+            print("Error al leer {nombre}: {e}")
+        
+# Si todo sale bien
+print("Archivos concatenados correctamente en ",archivo_salida)
+
+with open('Resultado.txt','r') as r:
+    contenido_archivoResultado = r.read() # Mostrar el contenido escrito
+    print(contenido_archivoResultado)
+
+    
+# Mostramos las primeras 10 lineas del archivo
+print('\n')
 
 
-# "Abrimos" 'archivo2.txt' y escribira lo siguiente (Estoy en el punto 15 y 16 de un ejercicio)
+# Mostramos las primeras y ultimas 10 lineas del 'Resultado.txt'
 
 
+# Comprobamos la fecho de creación de los archivos
 
 
-    # with open('archivo2.txt','w') as f:
-    #     f.write("esto es una prueba 1",'\n')
-    #     f.write("esto es una prueba 1",'\n')
-    #     f.write("esto es una prueba 1",'\n')
-    #     f.write("esto es una prueba 1",'\n')
-    #     f.write("esto es una prueba 1",'\n')
-    #     f.write("esto es una prueba 1",'\n')
+# Haz un update del fichero 'archivoFinal.txt'
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# print("FIN DEL SCRIPT. GRACIAS POR TODO")
-# print('\n')
+print("FIN DEL SCRIPT. GRACIAS POR TODO")
+print('\n')
--- borrarCarpetasYFicheros.py
+++ borrarCarpetasYFicheros.py
@@ -1,9 +1,10 @@
 import os, time, shutil
 
 # Borramos los directorios y ficheros que se crearon con el script de 'Inicio.py'
-print("Eliminando el fichero 'copia.txt'...")
+print("Eliminando el fichero 'copia.txt' y otros ficheros...")
 time.sleep(2)
 os.remove(".\copia.txt")
+os.remove(".\hola.py")
 print("Eliminando el directorio 'Segundo'...")
 time.sleep(2)
 shutil.rmtree(".\Segundo")
